06e29e0e11e31c58c57d12e1b3619e4f7718edf2
lgbouma <lgbouma@mit.edu>
2018-08-28 19:26:53 -0400

    Added Gaia DR2 astrometric excess filter call.

--- src/cks_age_exploration.py
+++ src/cks_age_exploration.py
@@ -252,6 +252,65 @@ def _apply_cks_IV_metallicity_study_filters(df):
     return sel
 
 
+def _apply_cks_IV_filters_plus_gaia_astrom_excess(df):
+    '''
+    also cut on gaia astrometric excess
+    '''
+
+    sel = np.isfinite(arr(df['giso_prad']))
+    sel &= arr(df['giso_prad']) < 32
+    sel &= np.isfinite(arr(df['giso_slogage']))
+
+    # apply all filters from table 1 of Petigura+ 2018
+    # Kp<14.2
+    sel &= np.isfinite(arr(df['kic_kepmag']))
+    sel &= arr(df['kic_kepmag']) < 14.2
+    # Teff=4700-6500K
+    sel &= np.isfinite(arr(df['cks_steff']))
+    sel &= arr(df['cks_steff']) < 6500
+    sel &= arr(df['cks_steff']) > 4700
+    # logg=3.9-5dex
+    sel &= np.isfinite(arr(df['giso_slogg']))
+    sel &= arr(df['giso_slogg']) < 5
+    sel &= arr(df['giso_slogg']) > 3.9
+    # P<350days
+    sel &= np.isfinite(arr(df['koi_period']))
+    sel &= arr(df['koi_period']) < 350
+    # not a false positive
+    is_fp = arr(df['cks_fp'])
+    sel &= ~is_fp
+    # not grazing (b<0.9)
+    sel &= np.isfinite(arr(df['koi_impact']))
+    sel &= arr(df['koi_impact']) < 0.9
+    # radius correction factor <5%, logical or not given in Furlan+2017 table.
+    sel &= ( (arr(df['fur17_rcorr_avg']) < 1.05) | (~np.isfinite(df['fur17_rcorr_avg']) ) )
+
+    # of the 1944 objects in the initial objects, 1606 have non-zero
+    # astrometric excess noise values.
+    # the associated distribution stats are:
+    #
+    # In [7]: df[df['astrometric_excess_noise_sig']!=0]['astrometric_excess_noise_sig'].describe()
+    # Out[7]: 
+    # count    3.380000e+02
+    # mean     3.710397e+02
+    # std      1.164272e+03
+    # min      6.486338e-16
+    # 25%      1.330256e-15
+    # 50%      6.488688e-01
+    # 75%      3.895363e+01
+    # max      9.709389e+03
+    # Name: astrometric_excess_noise_sig, dtype: float64
+    # 
+    # so we'll follow Rizzuto+ 2018 (ZEIT VIII) and use D > 10sigma as the
+    # cutoff.
+    sel &= (
+            ( arr(df['astrometric_excess_noise_sig']) >= 0 ) &
+            ( arr(df['astrometric_excess_noise_sig']) < 10 )
+    )
+
+    return sel
+
+
 def _main_cks_IV_filters():
 
     plot_wellmeasured = True
@@ -339,7 +398,9 @@ def _main_cks_IV_filters():
     # try to control for metallicity
     if plot_metallicity_controlled:
 
-        make_old_young_scatter(df, xparam='koi_period', metlow=-0.05, methigh=0.05)
+        make_old_young_scatter(df, xparam='koi_period', metlow=-0.05,
+                               methigh=0.05,
+                               savdir='../results/cks_age_scatter_percentiles/')
 
         binstr = 'binned_pm_pt05_FeH'
         metsel = arr(df['cks_smet']) <= 0.05
@@ -361,24 +422,161 @@ def _main_cks_IV_filters():
         mets = [(-0.25,-0.15),(-0.15,-0.05),(-0.05,0.05),(0.05,0.15),(0.15,0.25)]
         for met in mets:
             make_old_young_scatter(df, xparam='koi_period', metlow=met[0],
-                                   methigh=met[1])
+                                   methigh=met[1],
+                                   savdir='../results/cks_age_scatter_percentiles/')
             make_old_young_scatter(df, xparam='koi_dor', metlow=met[0],
-                                   methigh=met[1])
+                                   methigh=met[1],
+                                   savdir='../results/cks_age_scatter_percentiles/')
+
+        # Q: at fixed age, does metallicity affect the number of detected
+        # planets in the Rp vs Porb plane?
+        logages = [(9.2,9.4),(9.4,9.6),(9.6,9.8),(9.8,10.0)]
+        for logage in logages:
+            make_old_young_scatter(df, xparam='koi_period',
+                                   logagelow=logage[0], logagehigh=logage[1],
+                                   savdir='../results/cks_age_scatter_percentiles/')
+
+    plot_scatter(df, sel, 'giso_prad', 'cks_smet', True, False, is_cks=True,
+                 savdir='../results/cks_scatter_plots/')
+
+
+def _main_cks_IV_filters_plus_gaia_astrom_excess():
+
+    plot_wellmeasured = True
+    plot_janky = True
+    plot_boxplot = True
+    plot_stacked_histograms = True
+    plot_quartile_scatter = True
+    plot_octile_scatter = True
+    plot_metallicity_controlled = True
+    plot_metallicity_controlled_pcttiles = True
+
+    if plot_wellmeasured:
+        print('plotting well measured params...')
+    if plot_janky:
+        print('plotting janky params...')
+
+    df = _get_cks_data()
+    sel = _apply_cks_IV_filters_plus_gaia_astrom_excess(df)
+
+    # raw exploration plots
+    _ = arr(make_age_histograms_get_f_inds(
+                df[sel], savdir='../results/cks_gae_age_hist_precision/')
+    )
+
+    goodparams = ['koi_period', 'giso_prad', 'koi_count','koi_dor']
+    jankyparams = ['cks_smet']
+
+    for logy in [True, False]:
+        for logx in [True, False]:
+            for xparam in goodparams:
+                if not plot_wellmeasured:
+                    continue
+                plot_wellmeasuredparam(df, sel, xparam, logx, logy,
+                                       is_cks=True, savdir_append='_gae')
+
+    for logy in [True, False]:
+        for logx in [True, False]:
+            for xparam in jankyparams:
+                if not plot_janky:
+                    continue
+                plot_jankyparam(df, sel, xparam, logx, logy, is_cks=True,
+                                savdir_append='_gae')
+
+    if plot_boxplot:
+        print('making koi count boxplot')
+        make_boxplot_koi_count(df[sel])
+
+    ######################
+    # all CKS below here #
+    ######################
+    if plot_stacked_histograms:
+        print('plotting stacked histograms')
+        make_stacked_histograms(df[sel], logtime=True, xparam='aoverRstar',
+                                savdir='../results/cks_gae_age_explorn_stackedhist/')
+
+        make_stacked_histograms(df[sel], logtime=False, xparam='aoverRstar',
+                                savdir='../results/cks_gae_age_explorn_stackedhist/')
+
+        make_stacked_histograms(df[sel], logtime=True, xparam='period',
+                                savdir='../results/cks_gae_age_explorn_stackedhist/')
+
+        make_stacked_histograms(df[sel], logtime=False, xparam='period',
+                                savdir='../results/cks_gae_age_explorn_stackedhist/')
+
+        make_stacked_histograms(df[sel], logtime=False, xparam='radius',
+                                savdir='../results/cks_gae_age_explorn_stackedhist/')
+
+        make_stacked_histograms(df[sel], logtime=True, xparam='radius',
+                                savdir='../results/cks_gae_age_explorn_stackedhist/')
+
+    if plot_quartile_scatter:
+        make_quartile_scatter(df[sel], xparam='koi_period',
+                              savdir='../results/cks_gae_age_scatter_percentiles/')
+
+        make_quartile_scatter(df[sel], xparam='koi_dor',
+                              savdir='../results/cks_gae_age_scatter_percentiles/')
+
+    if plot_octile_scatter:
+        make_octile_scatter(df[sel], xparam='koi_period',
+                            savdir='../results/cks_gae_age_scatter_percentiles/')
+
+        make_octile_scatter(df[sel], xparam='koi_dor',
+                            savdir='../results/cks_gae_age_scatter_percentiles/')
+
+    # try to control for metallicity
+    if plot_metallicity_controlled:
+
+        make_old_young_scatter(df, xparam='koi_period', metlow=-0.05,
+                               methigh=0.05,
+                               savdir='../results/cks_gae_age_scatter_percentiles/')
+
+        binstr = 'binned_pm_pt05_FeH'
+        metsel = arr(df['cks_smet']) <= 0.05
+        metsel &= arr(df['cks_smet']) >= -0.05
+        outdir = '../results/cks_gae_age_plots/'
+        if not os.path.exists(outdir):
+            os.mkdir(outdir)
+        outpath = 'log_age_vs_cks_smet_VII_{:s}.pdf'.format(binstr)
+        logx, logy = False, True
+        plot_jankyparam(df, metsel, 'cks_smet', logx, logy, is_cks=True,
+                        outpdfpath=outpath, elw=0.2, ealpha=0.02)
+
+        _ = make_age_histograms_get_f_inds(df[metsel],
+                                           outstr='_smet_{:s}'.format(binstr),
+                                           logage=True)
+
+    if plot_metallicity_controlled_pcttiles:
+        # Q: at fixed metallicity, does age affect the number of detected
+        # planets in the Rp vs Porb plane?
+        # A: if at all, difficult to see.
+
+        mets = [(-0.25,-0.15),(-0.15,-0.05),(-0.05,0.05),(0.05,0.15),(0.15,0.25)]
+        for met in mets:
+            make_old_young_scatter(df, xparam='koi_period', metlow=met[0],
+                                   methigh=met[1],
+                                   savdir='../results/cks_gae_age_scatter_percentiles/')
+            make_old_young_scatter(df, xparam='koi_dor', metlow=met[0],
+                                   methigh=met[1],
+                                   savdir='../results/cks_gae_age_scatter_percentiles/')
 
         # Q: at fixed age, does metallicity affect the number of detected
         # planets in the Rp vs Porb plane?
         logages = [(9.2,9.4),(9.4,9.6),(9.6,9.8),(9.8,10.0)]
         for logage in logages:
             make_old_young_scatter(df, xparam='koi_period', logagelow=logage[0],
-                                   logagehigh=logage[1])
-            #make_old_young_scatter(df, xparam='koi_dor', metlow=met[0],
-            #                       methigh=met[1])
-
+                                   logagehigh=logage[1],
+                                   savdir='../results/cks_gae_age_scatter_percentiles/')
 
     plot_scatter(df, sel, 'giso_prad', 'cks_smet', True, False, is_cks=True,
                  savdir='../results/cks_scatter_plots/')
 
 
+
 if __name__ == '__main__':
 
-    _main_cks_IV_filters()
+    ## # make all the plots w/ CKS IV filters
+    ##  _main_cks_IV_filters()
+
+    # make all the plots w/ CKS IV filters + gaia astrometric excess cut
+    _main_cks_IV_filters_plus_gaia_astrom_excess()
